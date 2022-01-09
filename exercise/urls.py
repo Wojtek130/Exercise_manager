89f660a810dc62b141f5bd0247d62610ac476f3f
Wojtek <pimpek86@gmail.com>
2022-01-09 18:14:40 +0100

    templates created

--- exercise/urls.py
+++ exercise/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="exercise-home"),
+    path('about/', views.about, name="exercise-about"),
 ]
--- exercise/views.py
+++ exercise/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
-def home(request):
-    return HttpResponse("<h1>Blog Home<h1/>")
+exes = [{
+    "title" : "sitting"
+    },
+    {
+        "title" : "workout"
+    }
+    ]
+
+def  home(request):
+    context = {
+        "exes" : exes
+    }
+    return render(request, "exercise/home.html", context )
+
+def about(request):
+    return render(request, "exercise/about.html", {"page_title" : "About"})
